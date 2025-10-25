8d98456d59c385b3e2ea823d2c4940d661deb84e
HydroniumHA <simgeorges000@gmail.com>
2025-10-26 00:32:12 +0200

    Enable Action Cable request forgery protection configuration in application setup

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,6 @@ module ImgApiRubyRails
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.action_cable.disable_request_forgery_protection = true
   end
 end
