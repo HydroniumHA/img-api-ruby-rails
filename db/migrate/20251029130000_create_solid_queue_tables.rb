473afdab28f0195fe8918f7565247e0d5291ef4e
HydroniumHA <simgeorges000@gmail.com>
2025-10-29 12:30:11 +0100

    Add migration files for SolidQueue and SolidCache table creation

---
+++ db/migrate/20251029130000_create_solid_queue_tables.rb
@@ -0,0 +1,11 @@
+# db/migrate/20251029130000_create_solid_queue_tables.rb
+require "solid_queue/migration"
+class CreateSolidQueueTables < SolidQueue::Migration[5.2]
+  def change
+    SolidQueue::Migration.all.each do |migration|
+      next if migration.name == "SolidQueue::CreateTables"
+      up if migration.version.to_i <= version
+    end
+    SolidQueue::CreateTables.new.send(:change)
+  end
+end
---
+++ db/migrate/20251029130001_create_solid_cache_tables.rb
@@ -0,0 +1,10 @@
+# db/migrate/20251029130001_create_solid_cache_tables.rb
+require "solid_cache/migration"
+class CreateSolidCacheTables < SolidCache::Migration[5.2]
+  def change
+    SolidCache::Migration.all.each do |migration|
+      up if migration.version.to_i <= version
+    end
+    SolidCache::CreateTables.new.send(:change)
+  end
+end
