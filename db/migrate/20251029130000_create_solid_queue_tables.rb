79e6c21362dc32c7a7064b82fe0fcc7a11f4203c
HydroniumHA <simgeorges000@gmail.com>
2025-10-29 12:34:46 +0100

    Update migration classes to inherit from ActiveRecord::Migration[7.1] and remove unnecessary migration logic

--- db/migrate/20251029130000_create_solid_queue_tables.rb
+++ db/migrate/20251029130000_create_solid_queue_tables.rb
@@ -1,10 +1,6 @@
 # db/migrate/20251029130000_create_solid_queue_tables.rb
-class CreateSolidQueueTables < SolidQueue::Migration[5.2]
+class CreateSolidQueueTables < ActiveRecord::Migration[7.1]
   def change
-    SolidQueue::Migration.all.each do |migration|
-      next if migration.name == "SolidQueue::CreateTables"
-      up if migration.version.to_i <= version
-    end
     SolidQueue::CreateTables.new.send(:change)
   end
 end
--- db/migrate/20251029130001_create_solid_cache_tables.rb
+++ db/migrate/20251029130001_create_solid_cache_tables.rb
@@ -1,9 +1,6 @@
 # db/migrate/20251029130001_create_solid_cache_tables.rb
-class CreateSolidCacheTables < SolidCache::Migration[5.2]
+class CreateSolidCacheTables < ActiveRecord::Migration[7.1]
   def change
-    SolidCache::Migration.all.each do |migration|
-      up if migration.version.to_i <= version
-    end
     SolidCache::CreateTables.new.send(:change)
   end
 end
