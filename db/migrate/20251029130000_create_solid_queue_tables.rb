7c61c30fd0800b91b083fbc74a526ced300edace
HydroniumHA <simgeorges000@gmail.com>
2025-10-29 12:36:19 +0100

    Refactor SolidQueue and SolidCache migration files to directly call create_solid_queue_tables and create_solid_cache_tables methods

--- db/migrate/20251029130000_create_solid_queue_tables.rb
+++ db/migrate/20251029130000_create_solid_queue_tables.rb
@@ -1,6 +1,6 @@
 # db/migrate/20251029130000_create_solid_queue_tables.rb
 class CreateSolidQueueTables < ActiveRecord::Migration[7.1]
   def change
-    SolidQueue::CreateTables.new.send(:change)
+    create_solid_queue_tables
   end
 end
--- db/migrate/20251029130001_create_solid_cache_tables.rb
+++ db/migrate/20251029130001_create_solid_cache_tables.rb
@@ -1,6 +1,6 @@
 # db/migrate/20251029130001_create_solid_cache_tables.rb
 class CreateSolidCacheTables < ActiveRecord::Migration[7.1]
   def change
-    SolidCache::CreateTables.new.send(:change)
+    create_solid_cache_tables
   end
 end
